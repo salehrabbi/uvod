2d664bfb2df0a009085f4aaf4d94e4a1c4d7bc5b
Saleh Rabbi <33930388+salehrabbi@users.noreply.github.com>
2019-10-22 23:59:47 +0600

    date picker

--- resources/assets/script/main.js
+++ resources/assets/script/main.js
@@ -3,4 +3,6 @@
 //@prepros-append common/bootstrap.min.js
 //@prepros-append common/slick.min.js
 //@prepros-append common/boostrap-select.js
+//@prepros-append common/bootstrap-datetimepicker.js
+
 //@prepros-append common/custom.js
